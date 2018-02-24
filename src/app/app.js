12b0394265a8ead3039c567593c5bfdc4703fbb8
MOUNIKA <mounajakkani@gmail.com>
2018-02-23 22:23:53 -0800

    first commit

---
+++ src/app/app.js
@@ -0,0 +1,11 @@
+//
+import 'bootstrap/dist/css/bootstrap.min.css';
+// import 'bootstrap/dist/js/bootstrap.min.js';
+import "bootstrap";
+import $ from "jquery";
+import '../styles/app.scss';
+import Popper from "popper.js";
+import 'font-awesome/css/font-awesome.css';
+import 'slick-carousel';
+import '';
+// import 'lightgallery/dist/css/lightgallery.css';
---
+++ webpack.config.js
@@ -0,0 +1,48 @@
+var webpack = require('webpack');
+var path = require('path');
+
+module.exports = {
+  context: __dirname,
+  entry:[
+    './src/app/app.js'
+   ],
+  output:{
+    path: path.join(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+      test: /\.js$/,
+      loader: 'babel-loader',
+      include: path.join(__dirname,'src')
+      },
+
+    {
+      test: /\.css$/,
+      use: ['style-loader', 'css-loader']
+    },
+
+    {
+      test: /\.scss$/,
+      use: ['style-loader', 'css-loader', 'sass-loader']
+    },
+
+    {
+          test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+          loader: "url-loader?limit=10000&mimetype=application/font-woff"
+        },
+
+        {
+          test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+          loader: "file-loader"
+        },
+
+        {
+    test: /\.(jpe?g|png|gif|svg)$/i,
+    loader: "file-loader?name=/public/icons/[name].[ext]"
+}
+
+  ]
+}
+};
