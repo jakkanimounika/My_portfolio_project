460c8b1b89ab8de318c173f07817ebf45b3e4eb9
MOUNIKA <mounajakkani@gmail.com>
2018-02-28 20:17:16 -0800

    added styles

--- src/app/app.js
+++ src/app/app.js
@@ -1,9 +1,12 @@
-import $ from "jquery";
-import Popper from "popper.js";
-import "bootstrap";
+
+// import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import 'jquery/dist/jquery.min.js';
+// import $ from "jquery";
+// import Popper from "popper.js";
+import 'popper.js/dist/popper.min.js';
+import 'bootstrap/dist/js/bootstrap.min.js';
 import '../styles/app.scss';
-// import 'bootstrap/dist/js/bootstrap.min.js';
 import 'font-awesome/css/font-awesome.css';
 import 'slick-carousel';
 import 'google-fonts';
