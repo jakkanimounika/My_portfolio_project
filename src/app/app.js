3d7ae463b31a3221bd3bbcf25c6eb2dc3fb0097f
MOUNIKA <mounajakkani@gmail.com>
2018-03-01 21:40:55 -0800

    changes made

--- src/app/app.js
+++ src/app/app.js
@@ -1,14 +1,16 @@
 
-// import 'bootstrap';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'jquery/dist/jquery.min.js';
-// import $ from "jquery";
-// import Popper from "popper.js";
-import 'popper.js/dist/popper.min.js';
-import 'bootstrap/dist/js/bootstrap.min.js';
-import '../styles/app.scss';
+
 import 'font-awesome/css/font-awesome.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.min.js';
+import 'jquery/dist/jquery.min.js';
+import '../styles/app.scss';
+import $ from "jquery";
+import 'popper.js/dist/popper.min.js';
+import 'wowjs/dist/wow.min.js';
 import 'slick-carousel';
 import 'google-fonts';
 
+// import Popper from "popper.js";
+// import 'bootstrap';
 // import 'lightgallery/dist/css/lightgallery.css';
