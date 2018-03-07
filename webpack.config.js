4a06b9b0e370ace480dcc3ddf37db995ad501f21
MOUNIKA <mounajakkani@gmail.com>
2018-03-07 14:55:50 -0800

    fourth slide

--- webpack.config.js
+++ webpack.config.js
@@ -28,7 +28,8 @@ module.exports = {
       use: ['style-loader', 'css-loader', 'sass-loader']
     },
 
-    {
+//font-awesome
+       {
           test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
           loader: "url-loader?limit=10000&mimetype=application/font-woff"
         },
